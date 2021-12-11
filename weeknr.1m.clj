a4f6ac2fb9178285adf122ff54ca505ef36681cb
MMz <mmzsource@users.noreply.github.com>
2021-12-11 13:14:56 +0100

    Add calendar to my weeknr plugin

--- weeknr.1m.clj
+++ weeknr.1m.clj
@@ -10,15 +10,25 @@
 ;; Put this script inside the XBAR plugin folder. Most likely to be found here:
 ;; ~/Library/Application Support/xbar/plugins
 ;;
-;; Make sure the script has a name like weeknr.12h.clj where the .12h. is important
-;; because that's an interface to XBAR telling it it needs to run every 12 hours.
+;; Make sure the script has a name like weeknr.1m.clj where the .1m. is important
+;; because that's an interface to XBAR telling it it needs to run every minute.
 ;;
 
 ;; butlast to remove the newline at the end (the newline creates a new menu-item
 ;; in xbar and xbar will cycle between weeknr and a newline - not what we want)
 ;; apply str to then change all the loose chars into one string again.
+
 (def weeknr (apply str (butlast (:out (shell/sh "date" "+(wk%V)")))))
+
+;; telling xbar that every line that is printed should have the monospaced font
+;; 'Menlo Regular' and it should do no trimming. This in order to make the day
+;; numbers, day names and weeknumbers line out properly.
+
+(def month
+  (clojure.string/replace (:out (shell/sh "ncal" "-w")) #"\n" " | font='Menlo Regular' trim=false\n"))
 
 (println (str weeknr
               "\n---\n"
+              month
+              "---\n"
               "calendar | href=https://www.calendar-365.com"))
